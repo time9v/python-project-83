c61618933a699c04bd1656a5daa50c28e46103fc
time9v <lol.time9@gmail.com>
2024-09-18 16:56:42 +0300

    v0.0.2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 from dotenv import load_dotenv
 
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
