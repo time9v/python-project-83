096c47ea872f39290a26fad7d7ddb27e0f7db715
time9v <lol.time9@gmail.com>
2024-09-18 16:47:10 +0300

    Revert "v0.0.2"
    This reverts commit 934352159dea7eebc942da08fa9c880c48ba8c61.

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
